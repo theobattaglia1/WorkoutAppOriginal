474755ffbfd0f0744bdcbfc1e6d6f2838d6cb0f6
theobattaglia1 <125420400+theobattaglia1@users.noreply.github.com>
2025-03-25 15:10:12 -0700

    Delete WorkoutApp directory

--- WorkoutApp/Models/ExerciseDBModels.swift
+++
@@ -1,9 +0,0 @@
-struct RawExerciseDB: Codable {
-    let bodyPart: String
-    let equipment: String
-    let gifUrl: String
-    let id: String
-    let name: String
-    let target: String
-    let instructions: [String]
-}
--- WorkoutApp/Models/WorkoutRubric.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct WorkoutRubric: Codable {
-    let targetMuscles: [String]
-    let preferredEquipment: [String]
-    let movementPatterns: [String]
-    let intensity: String
-    let difficulty: Int
-}
